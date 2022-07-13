316363e42f1a274a64e071e323a7edb5b67de4a0
Julian Oseng <julianoseng@protonmail.com>
2022-07-13 17:01:07 -0500

    Initial commit and steps

---
+++ main.py
@@ -0,0 +1,9 @@
+# Display Window/GUI
+# Give browse button
+# Allow Toni to browse and find file on PC
+# One Toni selects file- let her click OK
+# Show warning window and ask Toni to confirm she wants to clean the Excel file
+# Once Toni confirms, remove rows 1-4
+# Remove columns D, E, G, H, I
+# Remove if column B does not have a check number in the box
+# Save a copy of the file within the same location
